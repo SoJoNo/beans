a6affe4f1895d1062d00e6fd38209e3e1eb60c83
Sopuruchi Noshiri <snoshiri@academic.rrc.ca>
2025-03-24 15:06:40 -0500

    Updated model woth associations and validations in coffee_review.rb.

--- app/models/coffee_review.rb
+++ app/models/coffee_review.rb
@@ -1,4 +1,10 @@
 class CoffeeReview < ApplicationRecord
   belongs_to :coffee_brand
   belongs_to :user
+
+  validates :rating, numericality: {
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 5
+  }
+  validates :review, length: { minimum: 20 }
 end
