9e41e58520aec21daff4a8dbc9e0d4ecc1e9c9ff
Sopuruchi Noshiri <snoshiri@academic.rrc.ca>
2025-03-24 15:24:00 -0500

    Created abiut page. Successful seeding.

--- app/models/coffee_review.rb
+++ app/models/coffee_review.rb
@@ -4,7 +4,6 @@ class CoffeeReview < ApplicationRecord
 
   validates :rating, numericality: {
     greater_than_or_equal_to: 0,
-    less_than_or_equal_to: 5
+    less_than_or_equal_to: 100
   }
-  validates :review, length: { minimum: 20 }
 end
