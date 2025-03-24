d3e2af7ee6228a2f4e5eb0db5fa52bca48c6dd19
Sopuruchi Noshiri <snoshiri@academic.rrc.ca>
2025-03-24 14:11:02 -0500

    Added content to seed.rb. Faker creates 150 fake reviews and 50 fake users. CSV is scraped from Kaggle CSV dataset for coffee shops and Yelp API.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'csv'
+require 'faker'
+require 'httparty'
+
+# Source 1: Kaggle CSV
+csv_data = CSV.read('db/coffee_shops.csv', headers: true)
+csv_data.each do |row|
+  CoffeeShop.create!(
+    name: row['name'],
+    city: row['city'],
+    country: row['country'],
+    rating: row['rating'].to_f,
+    specialty_coffee: row['specialty_coffee'] == 'Yes'
+  )
+end
+
+# Source 2: Yelp API (mock)
+CoffeeShop.first(5).each do |shop|
+  response = HTTParty.get("https://api.yelp.com/v3/businesses/#{shop.name.downcase.gsub(' ', '-')}",
+    headers: { "Authorization" => "Bearer YOUR_API_KEY" }
+  )
+  shop.update!(
+    hours: response['hours'],
+    price_range: response['price']
+  )
+end
+
+# Source 3: Faker (Users + Reviews)
+50.times do
+  user = User.create!(
+    username: Faker::Internet.username,
+    email: Faker::Internet.email
+  )
+
+  CoffeeShop.all.sample(5).each do |shop|
+    Review.create!(
+      content: Faker::Restaurant.review,
+      rating: rand(1..5),
+      coffee_shop: shop,
+      user: user
+    )
+  end
+end
