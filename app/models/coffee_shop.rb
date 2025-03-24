ab78ff7b5797a740509c0680da9644d45514a4ea
Sopuruchi Noshiri <snoshiri@academic.rrc.ca>
2025-03-24 14:33:31 -0500

    coffee_shop.rb updated to include updated validations.

--- app/models/coffee_shop.rb
+++ app/models/coffee_shop.rb
@@ -1,5 +1,5 @@
 class CoffeeShop < ApplicationRecord
-  has_many :reviews
   validates :name, presence: true
-  validates :rating, numericality: { in: 1..5 }
+  validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 end
