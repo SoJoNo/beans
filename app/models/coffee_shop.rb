bb41a3a6a857990f33374460c1599221c2344a5f
Sopuruchi Noshiri <snoshiri@academic.rrc.ca>
2025-03-24 15:32:43 -0500

    Added hierarchal navigation.

--- app/models/coffee_shop.rb
+++ app/models/coffee_shop.rb
@@ -2,4 +2,8 @@ class CoffeeShop < ApplicationRecord
   validates :name, presence: true
   validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
   validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+
+  def self.by_country
+    group(:country).count
+  end
 end
